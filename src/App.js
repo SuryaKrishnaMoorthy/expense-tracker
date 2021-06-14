6488f8da717017fa6210a72cde17854c7f3b1bc8
SURYA KRISHNA MOORTHY <ayrus2289@gmail.com>
2021-06-14 12:43:41 -0700

    add date style

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-import ExpenseItem from './components/ExpenseItem';
+import ExpenseItem from './components/ExpenseItem/ExpenseItem';
 
 function App() {
+  const expenses = [{
+    expenseDate: new Date(2021, 3, 31),
+    expenseTitle: 'Car Insurance',
+    expenseAmount: '$234.90'
+  },
+  {
+    expenseDate: new Date(2021, 2, 28),
+    expenseTitle: 'Toilet Paper',
+    expenseAmount: '$60'
+  },
+  {
+    expenseDate: new Date(2021, 1, 31),
+    expenseTitle: 'Rent',
+    expenseAmount: '$2034'
+  },
+  {
+    expenseDate: new Date(2020, 3, 17),
+    expenseTitle: 'Car Insurance',
+    expenseAmount: '$234.90'
+  }
+  ];
+
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem date={'March 9th 2021'} title={'Car Insurance'} amount={'$234.90'} />
+      <ExpenseItem date={ expenses[0].expenseDate } title={ expenses[0].expenseTitle } amount={ expenses[0].expenseAmount } />
+      <ExpenseItem date={ expenses[1].expenseDate } title={ expenses[1].expenseTitle } amount={ expenses[1].expenseAmount } />     
     </div>
   );
 }
